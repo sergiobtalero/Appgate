dc83f262112400970b189f4adb84217469cab45b
Sergio Bravo <sergio.bravo@monstar-lab.com>
2021-08-04 18:57:23 -0500

    Initial Commit

---
+++ Appgate/AppgateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppgateApp.swift
+//  Appgate
+//
+//  Created by Sergio Bravo on 4/08/21.
+//
+
+import SwiftUI
+
+@main
+struct AppgateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
